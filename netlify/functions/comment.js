1059fa290824d98f615f44cacff2fe32fef882df
Blackbird <sirius.su128@gmail.com>
2025-04-30 09:34:08 +0800

    Update comment.js
    改进邮件通知模板

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -4,11 +4,11 @@ const serverless = require('serverless-http');
 
 const app = Waline({
   env: 'netlify',
-  mailSubject: '{{parent.nick}}，您在博客「{{site.name}}」上的评论收到了回复',
+  mailSubject: '{{parent.nick}}，您在「Blackbird」博客上的留言收到了回复',
   mailTemplate: `
     <div style="border-top:2px solid #12ADDB;box-shadow:0 1px 3px #AAAAAA;line-height:180%;padding:0 15px 12px;margin:50px auto;font-size:12px;">
       <h2 style="border-bottom:1px solid #DDD;font-size:14px;font-weight:normal;padding:13px 0 10px 8px;">        
-        您在博客<a style="text-decoration:none;color: #12ADDB;" href="{{site.url}}" target="_blank">{{site.name}}</a>上的评论有了新的回复
+        您在<a style="text-decoration:none;color: #12ADDB;" href="{{site.url}}" target="_blank">「Blackbird」</a>博客上的评论有了新的回复
       </h2>
       <div style="padding:0 12px 0 12px;margin-top:18px">
 		<p>{{parent.nick}}，您曾发表评论：</p>
@@ -23,11 +23,11 @@ const app = Waline({
 	  </div>
 	  <br/>
     </div>`,
-  mailSubjectAdmin: '您的博客「{{site.name}}」收到了新评论',
+  mailSubjectAdmin: '您的博客「io-oi.dev」收到了新评论',
   mailTemplateAdmin: `
     <div style="border-top:2px solid #12ADDB;box-shadow:0 1px 3px #AAAAAA;line-height:180%;padding:0 15px 12px;margin:50px auto;font-size:12px;">
       <h2 style="border-bottom:1px solid #DDD;font-size:14px;font-weight:normal;padding:13px 0 10px 8px;">        
-        有人在<a style="text-decoration:none;color: #12ADDB;" href="{{site.url}}" target="_blank">{{site.name}}</a>发表了新的评论
+        有人在您的博客<a style="text-decoration:none;color: #12ADDB;" href="{{site.url}}" target="_blank">「io-oi.dev」</a>上发表了新的评论
       </h2>
       <div style="padding:0 12px 0 12px;margin-top:18px">
 		<p><strong>{{self.nick}}</strong> 评论说：</p>
